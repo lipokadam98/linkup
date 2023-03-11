558c1a8ff3a95bb8e7ff4ea572f2799214a186d5
lipokadam98 <lipokadam98@gmail.com>
2023-03-11 13:59:13 +0100

    teszt commit

--- linkup/src/app/auth/auth.interceptor.ts
+++ linkup/src/app/auth/auth.interceptor.ts
@@ -7,4 +7,5 @@ export class AuthInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     return next.handle(request);
   }
+
 }
